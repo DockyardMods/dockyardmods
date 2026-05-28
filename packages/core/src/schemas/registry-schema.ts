35aab22674772d8e63278e82aa3a02f0b4e78e4a
Thomas <77514919+TB516@users.noreply.github.com>
2026-05-28 00:15:03 -0400

    Updated registry schema key

--- packages/core/src/schemas/registry-schema.ts
+++ packages/core/src/schemas/registry-schema.ts
@@ -2,9 +2,12 @@ import { z } from 'zod';
 import { nonEmptyString } from './constants/non-empty-string.ts';
 import { modEntrySchema } from './mod-entry-schema.ts';
 
+export const registrySchemaUrl =
+	'https://raw.githubusercontent.com/DockyardMods/dockyardmods/main/packages/core/schemas/registry.schema.json';
+
 export const registrySchema = z
 	.strictObject({
-		$schema: z.literal('./schemas/registry.schema.json'),
+		$schema: z.literal(registrySchemaUrl),
 		version: z.literal(1),
 		currentGameVersionId: nonEmptyString,
 		mods: z.array(modEntrySchema)
