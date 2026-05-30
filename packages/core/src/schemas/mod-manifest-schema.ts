2c18207428bbebf623029de32153e7d97734b31b
Thomas <77514919+TB516@users.noreply.github.com>
2026-05-30 00:39:57 -0400

    Add icon to manifest schema

--- packages/core/src/schemas/mod-manifest-schema.ts
+++ packages/core/src/schemas/mod-manifest-schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { gameVersionId } from './constants/game-version-id.ts';
 import { modIdRegex } from './constants/mod-id-regex.ts';
 import { nonEmptyString } from './constants/non-empty-string.ts';
+import { relativeRepoPath } from './constants/relative-repo-path.ts';
 import { modDependencySchema } from './mod-dependency-schema.ts';
 import { modMediaSchema } from './mod-media-schema.ts';
 import { modPackageSchema } from './mod-package-schema.ts';
@@ -15,6 +16,7 @@ export const modManifestSchema = z
 		version: nonEmptyString,
 		authors: z.array(nonEmptyString).min(1),
 		gameVersionId,
+		icon: relativeRepoPath,
 		packages: z.array(modPackageSchema).min(1),
 		description: nonEmptyString.optional(),
 		tags: z.array(nonEmptyString).optional(),
